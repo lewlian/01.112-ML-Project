7ec196ab2a6e8522b2cd32c4e7692d53f13afa3c
joannasaw <thesawesomesaw@gmail.com>
2019-12-09 23:44:23 +0800

    Shift code from Jupyter notebooks to Python files

--- part3.py
+++ part3.py
@@ -1,11 +1,14 @@
 from collections import defaultdict
 import sys
 import json
+import part2 as em
+import numpy as np
+from itertools import groupby
 # defaultdict(int), if the key does not exist, it will initialize it to 0
 # access something that doesnt exist, default value given is 0
 
 def transition(file):
-    
+
     tagCount = defaultdict(int)
     tagToWordDictionary = defaultdict(lambda: defaultdict(int))
     lines = file.readlines()
@@ -29,11 +32,11 @@ def transition(file):
         # STOP CASE : line = "", prev_line = "tag"
         if (line.rstrip() == ""):
             w_i1 = previous_line.rstrip().rsplit(' ', 1)
-            
+
             tag_i1 = w_i1[1]
             tagToWordDictionary["STOP"][tag_i1] += 1
             tagCount["STOP"] += 1
-        
+
         # START CASE : prev_line = "", line = "tag"
         if (previous_line.rstrip() == ""):
             w_i = line.rstrip().rsplit(' ', 1)
@@ -47,16 +50,16 @@ def transition(file):
             #   strip all trailing spaces first and then split by white spaces limit to 1
             w_i = line.rstrip().rsplit(' ', 1)
             w_i1 = previous_line.rstrip().rsplit(' ', 1)
-            
+
             tag_i = w_i[1]
             tag_i1 = w_i1[1]
-            
+
             # case 3 = other words
-            
+
             tagToWordDictionary[tag_i][tag_i1] += 1
             tagCount[tag_i] += 1
 
-    
+
 
     mle = defaultdict(int)
 
@@ -67,18 +70,186 @@ def transition(file):
         #   Create a dictionary for each tag which is y-values
         mle[tag] = {}
         #   list(tagToWordDictionary[tag]) returns us ALL the words (all x-values)
-        
+
         for tag_i_minus_1 in list(tagToWordDictionary[tag]):
             mle[tag][tag_i_minus_1] = float(
                 tagToWordDictionary[tag][tag_i_minus_1]) / (tagCount[tag_i_minus_1])
 
     return mle
 
+def inner_viterbi(sentence, em_params, tr_params, tags):
+    words = sentence
+
+    # FORWARD
+    n = len(words)
+
+    T= [[0 for i in range(len(tags))] for j in range(n) ]
+
+    arg = [[0 for i in range(len(tags))] for j in range(n) ]
+
+
+    # base case/ first layer
+    for u in range(len(tags)):
+        word = words[0].rstrip("\n")
+        try:
+
+            a = tr_params[tags[u]]["START"]
+            b = em.getLikelihood(em_params, word, tags[u],w)
+
+            if b == 0:
+                nb = -sys.maxsize
+            else:
+                nb = np.log(b)
+
+            T[0][u] = np.log(a)+nb
+
+        except KeyError:
+
+            if b == 0:
+                nb = -sys.maxsize
+            else:
+                nb = np.log(b)
+            T[0][u] = -sys.maxsize + nb
+
+        except TypeError:
+            print('base has TypeError')
+            T[0][u] = -sys.maxsize
+
+
+    for k in range(1, n):
+        word = words[k].rstrip("\n")
+        for v in range(len(tags)):
+            find_max = []
+            find_arg = []
+
+            for u in range(len(tags)):
+                try:
+                    pilog = T[k-1][u]
+
+                    a = tr_params[tags[v]][tags[u]]
+                    b = em.getLikelihood(em_params, word, tags[v], w)
+
+                    if b==0:
+                        nb = -sys.maxsize
+                    else:
+                        nb = np.log(b)
+
+                    value = pilog+np.log(a)+nb
+                    arg_value = pilog+np.log(a)
+
+                except KeyError:
+                    if b == 0:
+                        nb = -sys.maxsize
+                    else:
+                        nb = np.log(b)
+
+                    value = pilog - sys.maxsize + nb
+                    arg_value = pilog - sys.maxsize
+
+
+                find_max.append(value)
+
+                find_arg.append(arg_value)
+
+
+            T[k][v] = max(find_max)
+            arg[k][v] = find_arg.index(max(find_arg))+1
+
+
+
+    #BACKWARD
+    index = [0]*n
+    y = [0]*n
+
+    # Find maximum in last layer
+    last_layer = T[n-1]
+    final_check = []
+    for i in range(len(last_layer)):
+        pilog = last_layer[i]
+        try:
+            a = tr_params["STOP"][tags[i]]
+            value = pilog + np.log(a)
+        except KeyError:
+            value = pilog - sys.maxsize
+        final_check.append(value)
+
+    # Find last tag
+    true_max = -sys.maxsize
+    for m in range(len(final_check)):
+        value = final_check[m]
+        if value > true_max:
+            true_max = value
+    if true_max == -sys.maxsize:
+        index[n-1] = index_unknown
+        y[n-1] = tags[index[n-1]]
+    else:
+        index[n-1] = final_check.index(true_max)+1
+        y[n-1] = tags[final_check.index(true_max)]
+
+    # Find the rest of values using stored arg table
+    for j in range(n-1,0,-1):
+        index[j-1] = arg[j][index[j]-1]
+        y[j-1] = tags[index[j-1]-1]
+
+    return y,T,arg
+
+def viterbi(file, em_params, tr_params, tags):
+    file_list = file.readlines()
+    sentences = [list(group) for k, group in groupby(file_list, lambda x: x == "\n") if not k]
+    total_y = []
+    for i in range(len(sentences)):
+        if i%10 == 0:
+            print(i)
+        sentence = sentences[i]
+        y,T,arg = inner_viterbi(sentence, em_params, tr_params, tags)
+        total_y += y
+        total_y += [0]
+    return total_y
+
+def predictions_file(inputFile, outputfile, y):
+    f = open(outputfile, 'w+', encoding="utf8")
+
+    finput = open(inputFile, 'r', encoding="utf8")
+    lines = finput.readlines()
+    print(len(lines))
+    print(len(y))
+    for i in range(len(lines)):
+        if (lines[i].rstrip() != ""):
+            word = lines[i].rstrip()
+            # print(word, y[i+1])
+            if (word == '\n'):
+                f.write(word)
+            else:
+                f.write(word+" "+y[i]+"\n")
+
+        else:
+            f.write('\n')
+    f.close()
+    print('Finished writing to File.')
 
 
 if __name__ == "__main__":
-    
-    filePath = "/Users/nashitaabd/Documents/GitHub/01.112-ML-Project/EN/train"
-    mle = transition(
-        open(filePath, "r", encoding="utf8"))
-    
+
+    dataset = "EN"
+    file_em = dataset+"/train"
+    em_params, tagCount, w = em.emmissionWithSmoothing(open(file_em, "r", encoding="utf8"), 3)
+
+    tr_params = transition(open(file_em, "r", encoding="utf8"))
+    tags = list(tr_params)
+    tags.remove("STOP")
+    tags.remove("START")
+
+    filePath = dataset+"/dev.in"
+    fileout = dataset+"/dev.p3.out"
+
+    common = ''
+    max_count = 0
+    for k,v in tagCount.items():
+        if v > max_count:
+            print(v)
+            common = k
+            max_count = v
+    index_unknown = tags.index(common)
+
+    y = viterbi((open(filePath, "r", encoding="utf8")), em_params, tr_params, tags)
+    predictions_file(filePath, fileout, y)
---
+++ part4.py
@@ -0,0 +1,188 @@
+import sys
+import part3 as tr
+import part2 as em
+import numpy as np
+from itertools import groupby
+import copy
+from collections import Counter
+
+def inner_viterbi_k(sentence, em_params, tr_params, tags, k):
+    words = sentence
+
+    # FORWARD
+    n = len(words)
+
+    T= [[[0] for i in range(len(tags))] for j in range(n) ]
+
+    arg = [[[""] for i in range(len(tags))] for j in range(n) ]
+
+
+    # base case/ first layer
+    for u in range(len(tags)):
+        word = words[0].rstrip("\n")
+
+        try:
+
+            a = tr_params[tags[u]]["START"]
+            b = em.getLikelihood(em_params, word, tags[u],w)
+
+            if b == 0:
+                nb = -sys.maxsize
+            else:
+                nb = np.log(b)
+
+            T[0][u] = [np.log(a)+nb]
+
+        except KeyError:
+
+            if b == 0:
+                nb = -sys.maxsize
+            else:
+                nb = np.log(b)
+            T[0][u] = [-sys.maxsize + nb]
+
+        except TypeError:
+            print('base has TypeError')
+            T[0][u] = [-sys.maxsize]
+
+
+    for j in range(1, n):
+        word = words[j].rstrip("\n")
+
+        for v in range(len(tags)):
+            dic = {}
+
+            for u in range(len(tags)):
+                for t in range(len(T[j-1][0])):
+                    try:
+                        pilog = T[j-1][u][t]
+
+                        a = tr_params[tags[v]][tags[u]]
+                        b = em.getLikelihood(em_params, word, tags[v], w)
+
+                        if b==0:
+                            nb = -sys.maxsize
+                        else:
+                            nb = np.log(b)
+
+                        value = pilog+np.log(a)+nb
+
+                    except KeyError:
+                        if b == 0:
+                            nb = -sys.maxsize
+                        else:
+                            nb = np.log(b)
+
+                        value = pilog - sys.maxsize + nb
+
+                    path = arg[j-1][u][t]
+
+                    dic[(u+1,path)] = value
+
+            kt = Counter(dic)
+            top_k = kt.most_common(k)
+            top_k_prob = []
+            top_k_path = []
+
+            for i in range(k):
+                top_k_prob.append(top_k[i][1])
+                prev_path = top_k[i][0][1]
+                new_path = prev_path + "," + str(top_k[i][0][0])
+                top_k_path.append(new_path)
+
+            T[j][v] = top_k_prob
+            arg[j][v] = top_k_path
+
+
+
+    #BACKWARD
+
+    index = [0]*n
+    y = [0]*n
+
+    # Find k highest in last layer
+    last_layer = T[n-1]
+    final_check = {}
+    for i in range(len(last_layer)):
+        for t in range(k):
+            pilog = last_layer[i][t]
+            try:
+                a = tr_params["STOP"][tags[i]]
+                value = pilog + np.log(a)
+            except KeyError:
+                value = pilog - sys.maxsize
+
+            final_check[(i,t)] = value
+    final = Counter(final_check)
+    top_final = final.most_common(k)
+
+    # Find k best paths in stored arg table
+    best_k_paths = []
+    for m in range(k):
+        i = top_final[m][0][0]
+        t = top_final[m][0][1]
+        prev_path = arg[n-1][i][t]
+        total_path = prev_path+","+str(i+1)
+
+        total_path_tags = []
+        total_path_list = total_path.split(",")
+        total_path_list = total_path_list[1:]
+
+        for l in range(len(total_path_list)):
+            total_path_tags.append(tags[int(total_path_list[l])-1])
+        best_k_paths.append(total_path_tags)
+
+    return best_k_paths,T,arg
+
+def viterbi_kth(file, em_params, tr_params, tags, k):
+    file_list = file.readlines()
+    sentences = [list(group) for k, group in groupby(file_list, lambda x: x == "\n") if not k]
+    total_y = []
+    for i in range(len(sentences)):
+        if i%10 == 0:
+            print(i)
+        sentence = sentences[i]
+        y,T,arg = inner_viterbi_k(sentence, em_params, tr_params, tags, k)
+        if len(y)!= k:
+            print("Error: y from inner_viterbi_k is of length:",len(y),"while k is",k)
+        total_y += y[k-1]
+        total_y += [0]
+    return total_y
+
+def predictions_file(inputFile, outputfile, y):
+    f = open(outputfile, 'w+', encoding="utf8")
+
+    finput = open(inputFile, 'r', encoding="utf8")
+    lines = finput.readlines()
+    print(len(lines))
+    print(len(y))
+    for i in range(len(lines)):
+        if (lines[i].rstrip() != ""):
+            word = lines[i].rstrip()
+            # print(word, y[i+1])
+            if (word == '\n'):
+                f.write(word)
+            else:
+                f.write(word+" "+y[i]+"\n")
+
+        else:
+            f.write('\n')
+    f.close()
+    print('Finished writing to File.')
+
+if __name__ == "__main__":
+
+    dataset = "EN"
+    file_em = dataset+"/train"
+    em_params, tagCount, w = em.emmissionWithSmoothing(open(file_em, "r", encoding="utf8"), 3)
+
+    tr_params = tr.transition(open(file_em, "r", encoding="utf8"))
+    tags = list(tr_params)
+    tags.remove("STOP")
+    tags.remove("START")
+
+    filePath = dataset+"/dev.in"
+    fileout = dataset+"/dev.p4.out"
+
+    y = viterbi_kth((open(filePath, "r", encoding="utf8")), em_params, tr_params, tags, 7)
+    predictions_file(filePath, fileout, y)

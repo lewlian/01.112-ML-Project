1653bd469c17051bb10e8886c060ba070519f8aa
Sean Lew <seanlewtengsiong@gmail.com>
2019-12-10 12:41:51 +0800

    added results for p5.dev.out

--- part5.py
+++ part5.py
@@ -248,14 +248,14 @@ def get_features(word, prev_word, prev_tag, prev2_tag, prev2_word, next_word, ne
 
 
 # RUNNING THE CODE
-output, tag_counts = parse_feature_tag_pairs('./AL/', 'train')
+output, tag_counts = parse_feature_tag_pairs('./EN/', 'train')
 test = perceptronTagger(tag_counts)
 
 # Number of iterations to run perceptron
 n = 20
 model_weights = test.train(n, output)
 fileIn = './EN/dev.in'
-fileOut = './EN/devavg.p5.out'
+fileOut = './EN/dev.p5.out'
 parse_predict_test_file(fileIn, fileOut, test)
 
 
